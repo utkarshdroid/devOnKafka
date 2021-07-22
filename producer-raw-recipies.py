bf27802805e9d3bfcf6cc5e0ecd3101a8cb49f07
Utkarsh Sharma <utkarshdroid@gmail.com>
2021-07-22 20:03:13 +0530

    value in py

--- producer-raw-recipies.py
+++ producer-raw-recipies.py
@@ -5,11 +5,11 @@ from bs4 import BeautifulSoup
 from kafka import KafkaProducer
 
 
-def publish_message(producer_instance, topic_name, key, value):
+def publish_message(producer_instance, topicname, key, value):
     try:
         key_bytes = bytes(key, encoding='utf-8')
         value_bytes = bytes(value, encoding='utf-8')
-        producer_instance.send(topic_name, key=key_bytes, value=value_bytes)
+        producer_instance.send(topicname, key=key_bytes, value=value_bytes)
         producer_instance.flush()
         print('Message published successfully.')
     except Exception as ex:
